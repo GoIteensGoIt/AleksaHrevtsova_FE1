edaf510a238128be7c00f6fa7683a8827ea5fb3f
AndriiShnurkov <Shans12.84@ukr.net>
2020-03-03 22:20:06 +0200

    added project for deploy

---
+++ build/js/scripts.js
@@ -0,0 +1,17 @@
+// (function() {
+//   document.addEventListener('DOMContentLoaded', ready);
+//   function ready() {
+//     var button = document.querySelector('.first-section-header__button');
+//     var navigation = document.querySelector('.navigation');
+//     button.addEventListener('click', function(e) {
+//       button.classList.toggle('active');
+//       navigation.classList.toggle('active');
+//     });
+//     console.log('button', button);
+//     navigation.addEventListener('click', function() {
+//       button.classList.remove('active');
+//       navigation.classList.remove('active');
+//     });
+//   }
+// })();
+"use strict";
